10674b3fb7030eb51a46cbc619993098929a89b8
khalik8354 <mosesmashaa00@gmail.com>
2025-02-01 17:19:10 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
   packname: 'Knight',
   author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botName: "Khalik Bot",
+  botOwner: 'khalik', // Your name
+  ownerNumber: '254708272644', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
